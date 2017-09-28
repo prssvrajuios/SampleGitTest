401dfda236ba888a61ed93b5795592e9fd0ab0df
Apple <Siva@Apples-iMac.local>
2017-09-28 20:44:36 +0530

    from iMac

--- SampleGitTest/ViewController.swift
+++ SampleGitTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Mohan sir")
+        print("Hello")
     }
 
     override func didReceiveMemoryWarning() {
